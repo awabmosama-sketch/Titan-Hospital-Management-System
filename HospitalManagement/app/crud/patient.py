5750fb3c8d0fbb91930d7d4b4f5937501efa3e52
Awab <awabmosama@gmail.com>
2026-07-06 18:37:01 +0500

    crud for patient is done

--- HospitalManagement/app/crud/patient.py
+++ HospitalManagement/app/crud/patient.py
@@ -15,3 +15,34 @@ def create_patient(db: Session, patient_in: PatientCreate):
     db.commit()
     db.refresh(db_patient)
     return db_patient
+
+def get_patient(db : Session , patient_id : int):
+    return db.query(Patient).filter(Patient.id == patient_id).first()
+
+
+def get_all_patients(db : Session,skip:int = 0,limit : int = 100):
+    return db.query(Patient).offset(skip).limit(limit).all()
+
+def update_paitent(db : Session,patient_id : int , patient_update : PatientUpdate):
+    db_patient = db.query(Patient).filter(Patient.id == patient_id).first()
+    if not db_patient:
+        return None
+    
+    update_data = patient_update.dict(exclude_unset=True)
+
+    for key , value in update_data.items():
+        setattr(db_patient,key,value)
+
+    db.commit()
+    db.refresh(db_patient)
+    return db_patient
+
+def delete_patient(db : Session,patient_id : int):
+    db_patient = db.query(Patient).filter(Patient.id == patient_id).first()
+    if not db_patient:
+        return None
+
+    db.delete(db_patient)
+    db.commit()
+    return db_patient
+
